a9bf7e72ac778016282c6d20cc9aa9504a53ee13
Amber Lin <410103037@gms.ndhu.edu.tw>
2022-06-05 22:45:20 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './App.css';
+import Nav from './components/Nav';
+import Products from './pages/Products';
+import ProductDetail from "./pages/ProductDetail";
+import Articles from './pages/Article';
+import ArticleDetail from './pages/ArticleDetail';
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:productId" element={<ProductDetail />} />
+        <Route path="article" element={<Articles />} />
+        <Route path="article/:articleId" element={<ArticleDetail />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default App;
